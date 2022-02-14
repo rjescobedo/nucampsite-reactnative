ef93ebd8771d75df49a6391ad25952b54feac64c
Raul Escobedo <rjescobedo@gmail.com>
2022-02-14 04:38:14 -0600

    Week 4 Workshop

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 class LoginTab extends Component {
 
@@ -168,6 +169,33 @@ class RegisterTab extends Component {
         }
     }
 
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [
+                { width: 400 },
+                { compress: 1, format: SaveFormat.PNG}
+            ]
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+    }
+
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -194,6 +222,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button 
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
